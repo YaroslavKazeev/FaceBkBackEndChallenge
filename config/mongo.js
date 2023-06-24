962659acfcd38df76708a0dab22061a08c0954d2
YaroslavKazeyev <yaroslav.kazeyev@gmail.com>
2023-06-24 20:50:21 +0200

    Added the picture and deleted MongoDB credentials
    Added the picture and deleted MongoDB credentials

--- config/mongo.js
+++ config/mongo.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yaroslavkazeyev:tFva9V1Ex3Vxv0uz@cluster0.zzdkhgp.mongodb.net/')
+mongoose.connect('mongodb+srv://XXX:XXX@cluster0.zzdkhgp.mongodb.net/')
 .then(()=> console.log('DB is connected'))
 .catch(err=> console.log(err))
